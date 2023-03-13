503ffdf7403ec7bccc155f622297da4604235a82
Domenico Daddato <123358216+domenicodaddatoo@users.noreply.github.com>
2023-03-13 10:58:32 +0100

    Add files via upload

---
+++ cognomi.py
@@ -0,0 +1,9 @@
+a= 10
+list_cognomi=[ ]
+for a in range (0,a):
+    cognomi=str(input("inserisci un cognome: "))
+    list_cognomi.append(cognomi)
+for frase in list_cognomi:
+    print('ciao ', frase)
+    
+    
